0108ff62474b3f3ec81822e24587b6dec364a7d1
MUHAMMAD ALI ZAFAR <98279815+AliZaffar-ai@users.noreply.github.com>
2023-12-28 23:24:52 +0500

    Add files via upload

---
+++ Model_EmotionDetector.py
@@ -0,0 +1,107 @@
+
+import cv2
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.models import model_from_json
+from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import confusion_matrix, classification_report,ConfusionMatrixDisplay
+
+train_data_gen = ImageDataGenerator(rescale=1./255)
+validation_data_gen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_data_gen.flow_from_directory(
+        '/Users/apple/NEWFOLDER/Emotion-detection-main/data/train',
+        target_size=(48, 48),
+        batch_size=64,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+validation_generator = validation_data_gen.flow_from_directory(
+        '/Users/apple/NEWFOLDER/Emotion-detection-main/data/test',
+        target_size=(48, 48),
+        batch_size=64,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+# create model structure
+emotion_model = Sequential()
+
+emotion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
+emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Flatten())
+emotion_model.add(Dense(1024, activation='relu'))
+emotion_model.add(Dropout(0.5))
+emotion_model.add(Dense(7, activation='softmax'))
+
+cv2.ocl.setUseOpenCL(False)
+
+emotion_model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.0001), metrics=['accuracy'])
+
+
+# Train the neural network/model
+emotion_model_info = emotion_model.fit(
+        train_generator,
+        steps_per_epoch=28709 // 64,
+        epochs=4,
+        validation_data=validation_generator,
+        validation_steps=7178 // 64)
+
+# save model structure in jason file
+model_json = emotion_model.to_json()
+with open("/Users/apple/NEWFOLDER/Emotion-detection-main/model/emotion_model.json", "w") as json_file:
+    json_file.write(model_json)
+
+# save trained model weight in .h5 file
+emotion_model.save_weights('/Users/apple/NEWFOLDER/Emotion-detection-main/model/emotion_model.h5')
+
+
+emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+
+# load json and create model
+json_file = open('model/emotion_model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+emotion_model = model_from_json(loaded_model_json)
+
+# load weights into new model
+emotion_model.load_weights("model/emotion_model.h5")
+print("Loaded model from disk")
+
+# Initialize image data generator with rescaling
+test_data_gen = ImageDataGenerator(rescale=1./255)
+
+# Preprocess all test images
+test_generator = test_data_gen.flow_from_directory(
+        'data/test',
+        target_size=(48, 48),
+        batch_size=64,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+# do prediction on test data
+predictions = emotion_model.predict(test_generator)
+
+
+
+print("-----------------------------------------------------------------")
+# confusion matrix
+c_matrix = confusion_matrix(test_generator.classes, predictions.argmax(axis=1))
+print(c_matrix)
+cm_display = ConfusionMatrixDisplay(confusion_matrix=c_matrix, display_labels=emotion_dict)
+cm_display.plot(cmap=plt.cm.Blues)
+plt.show()
+
+# Classification report
+print("-----------------------------------------------------------------")
+print(classification_report(test_generator.classes, predictions.argmax(axis=1)))
